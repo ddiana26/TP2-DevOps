65d8836274940c8326ce2d8b853b203da7899ee5
Diana Dumitrescu <diana.dumitrescu2026@campus-eni.fr>
2026-08-11 10:55:07 +0200

    Ajout des tests d’intégration pour les endpoints Task

--- src/config/db.config.js
+++ src/config/db.config.js
@@ -1,6 +1,7 @@
 module.exports = {
   HOST: process.env.DB_HOST || 'localhost',
   USER: process.env.DB_USER || 'root',
+  PORT: process.env.DB_PORT || 3306,
   PASSWORD: process.env.DB_PASSWORD || '',
   DB: process.env.DB_NAME || 'todolist_db',
   dialect: process.env.DB_DIALECT || 'mysql',
